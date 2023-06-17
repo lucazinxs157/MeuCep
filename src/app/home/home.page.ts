8f496f174403586ba712ccac6973d1a9249ec5a4
Lukinhaz <lucaswiacek6@gmail.com>
2023-06-16 21:56:06 -0300

    novo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { AlertController, NavController } from '@ionic/angular';
+import {
+  AlertController,
+  NavController,
+  ToastController,
+} from '@ionic/angular';
 import { MeuCepService } from '../services/meucep.service';
 
 @Component({
@@ -8,14 +12,69 @@ import { MeuCepService } from '../services/meucep.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(
-    public mensagem: AlertController,
-    public nav: NavController,
-    public cep: MeuCepService
-  ) { }
+  dados: any = {};
 
+  endereco = {
+    endereco: '',
+    numero: '',
+    complemento: '',
+    bairro: '',
+    cep: '',
+    cidade: '',
+    estado: '',
+  };
+
+  constructor(
+    public mensagem: ToastController,
+    public nav: NavController,
+    private cep: MeuCepService
+  ) {}
 
   searchCEP(evento: any) {
-    const cepDig = evento.detail.valeu;
+    const cepDig = evento.detail.value;
+    if (cepDig.length == 8) {
+      this.cep
+        .localizaCep(cepDig)
+        .then((resp) => {
+          this.dados = resp;
+          if (!this.dados || this.dados.erro) {
+            this.exibeToast('CEP não encotrado', 'warning');
+          } else {
+            this.endereco.endereco = this.dados.logradouro;
+            this.endereco.bairro = this.dados.bairro;
+            this.endereco.cidade = this.dados.localidade;
+            this.endereco.estado = this.dados.uf;
+            
+          }
+        })
+        .catch(() => {
+          this.exibeToast('CEP não encotrado', 'warning');
+        });
+    }
+  }
+
+  cadastrar() {
+    if(this.endereco.cidade == '' ||
+      this.endereco.estado == '' ||    
+      this.endereco.endereco == '' ||   
+      this.endereco.cep == '' ||    
+      this.endereco.bairro == ''     
+    ){
+      this.exibeToast('Preenche os campos necessarios', 'danger');
+    }else{
+      this.nav.navigateForward('conclusão')
+  
+      }  }
+
+  async exibeToast(msg: string, cor: string) {
+    const toast = await this.mensagem.create({
+      message: msg,
+      duration: 2000,
+      position: 'top',
+      animated: true,
+      color: cor,
+    });
+
+    toast.present();
   }
 }
--- src/app/services/meucep.service.ts
+++ src/app/services/meucep.service.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
-
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MeuCepService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  localizaCep(cod: string) {
+    const url = `https://viacep.com.br/ws/${cod}/json`;
+
+    const header = {
+      headers: new HttpHeaders().set('Content-Type', 'application/json'),
+    };
+    return this.http.get(url, header).toPromise();
+  }
 }
